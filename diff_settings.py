ff16bcd8d58a3bc366eceae415e8c5ce75638f6e
Daniel R <47796739+polybiusproxy@users.noreply.github.com>
2023-08-14 12:36:00 +0200

    diff: fix baseimg path

--- diff_settings.py
+++ diff_settings.py
@@ -1,6 +1,6 @@
 def apply(config, args):
     config["arch"] = "mipsel"
-    config['baseimg'] = f'MAIN_T.EXE'
+    config['baseimg'] = f'iso/MAIN_T.EXE'
     config['myimg'] = f'build/MAIN_T.EXE'
     config['mapfile'] = f'build/vib-ribbon.map'
     config['source_directories'] = [f'src/game', 'include', f'asm/game']
