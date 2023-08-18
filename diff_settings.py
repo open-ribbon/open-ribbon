54d4feef5b405865cc6d6089d171f05cc5f51985
Daniel R <47796739+polybiusproxy@users.noreply.github.com>
2023-08-18 11:26:05 +0200

    delete the rest

--- diff_settings.py
+++
@@ -1,6 +0,0 @@
-def apply(config, args):
-    config["arch"] = "mipsel"
-    config['baseimg'] = f'iso/MAIN_T.EXE'
-    config['myimg'] = f'build/MAIN_T.EXE'
-    config['mapfile'] = f'build/vib-ribbon.map'
-    config['source_directories'] = [f'src/game', 'include', f'asm/game']
--- include/common.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-#include "include_asm.h"
-#include "types.h"
-
-#endif
